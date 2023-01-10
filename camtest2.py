807bf9c2e4915f8eef9b25280e8727e85ece1e6e
Kunal Aneja <kunal.aneja101@gmail.com>
2023-01-10 18:04:05 -0500

    clean up files

--- camtest2.py
+++
@@ -1,8 +0,0 @@
-
-import gst
-
-pipeline = gst.parse_launch("""
-v4l2src ! decodebin ! ffmpegcolorspace ! pngenc ! filesink location="%s"
-""" % sys.argv[-1])
-
-pipeline.set_state(gst.STATE_PLAYING)
