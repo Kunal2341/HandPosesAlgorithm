f31585db22b718e36a1ef416990c0a5248d3a569
Kunal Aneja <kunal.aneja101@gmail.com>
2023-01-17 11:27:07 -0500

    text on images

--- augmentingImg.py
+++ augmentingImg.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageEnhance, ImageOps
+from PIL import Image, ImageEnhance, ImageOps, ImageFont, ImageDraw
 import os
 #mediapipe = "mediapipe-results-all/"
 #mmpose = "mmpose-results-images-all/"
@@ -59,30 +59,50 @@ makeFolder(rotatedImg45)
 makeFolder(rotatedImg90)
 makeFolder(grayscaled)
 
+def writeTextChange(image, text):
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 75)
+    #font = ImageFont.load_default(size=24)
+    draw.text((10, 10), text, (255,255,255),font=font)
+    return image
+
 for img in os.listdir(orginal):
     im = Image.open(orginal + img)
     enhancer = ImageEnhance.Brightness(im)
     im_output125 = enhancer.enhance(1.25)
     im_output150 = enhancer.enhance(1.5)
     im_output175 = enhancer.enhance(1.75)
+    writeTextChange(im_output125, img[:-4] + " - Brighten by 25%")
+    writeTextChange(im_output150, img[:-4] + " - Brighten by 50%")
+    writeTextChange(im_output175, img[:-4] + " - Brighten by 75%")
     im_output125.save(mainFolderName + "/" + brightFolder125 + "/" + img[:-4] + "-" + brightFolder125 + ".jpg")
     im_output150.save(mainFolderName + "/" + brightFolder150 + "/" + img[:-4] + "-" + brightFolder150 + ".jpg")
     im_output175.save(mainFolderName + "/" + brightFolder175 + "/" + img[:-4] + "-" + brightFolder175 + ".jpg")
     im_darken125 = enhancer.enhance(0.75)
     im_darken150 = enhancer.enhance(0.50)
     im_darken175 = enhancer.enhance(0.25)
+    writeTextChange(im_darken125, img[:-4] + " - Darken by 25%")
+    writeTextChange(im_darken150, img[:-4] + " - Darken by 50%")
+    writeTextChange(im_darken175, img[:-4] + " - Darken by 75%")
     im_darken125.save(mainFolderName + "/" + darkenFolder125 + "/" + img[:-4] + "-" + darkenFolder125 + ".jpg")
     im_darken150.save(mainFolderName + "/" + darkenFolder150 + "/" + img[:-4] + "-" + darkenFolder150 + ".jpg")
     im_darken175.save(mainFolderName + "/" + darkenFolder175 + "/" + img[:-4] + "-" + darkenFolder175 + ".jpg")
     im_flipped = ImageOps.flip(im)
+    writeTextChange(im_flipped, img[:-4] + " - Flipped Image")
     im_flipped.save(mainFolderName + "/" + flippedFolder + "/" + img[:-4] + "-" + flippedFolder + ".jpg")
     im_contrast100 = change_contrast(im, 100)
     im_contrast200 = change_contrast(im, 200)
+    writeTextChange(im_contrast100, img[:-4] + " - Changed Contrast 100")
+    writeTextChange(im_contrast200, img[:-4] + " - Changed Contrast 200")
     im_contrast100.save(mainFolderName + "/" + contrast100 + "/" + img[:-4] + "-" + contrast100 + ".jpg")
     im_contrast200.save(mainFolderName + "/" + contrast200 + "/" + img[:-4] + "-" + contrast200 + ".jpg")
-    im.save(mainFolderName + "/" + resolution50 + "/" + img[:-4] + "-" + resolution50 + ".jpg", quality=50)
+    im_resolution = im
+    writeTextChange(im_resolution, img[:-4] + " - Resolution by 50%")
+    im_resolution.save(mainFolderName + "/" + resolution50 + "/" + img[:-4] + "-" + resolution50 + ".jpg", quality=50)
     im_rotate45 = im.rotate(45)
     im_rotate90 = im.rotate(90)
+    writeTextChange(im_rotate45, img[:-4] + " - Rotate by 45*")
+    writeTextChange(im_rotate90, img[:-4] + " - Rotate by 90*")
     im_rotate45.save(mainFolderName + "/" + rotatedImg45 + "/" + img[:-4] + "-" + rotatedImg45 + ".jpg")
     im_rotate90.save(mainFolderName + "/" + rotatedImg90 + "/" + img[:-4] + "-" + rotatedImg90 + ".jpg")
     im_grayscale = im.convert('L')
--- stitchAugmentedImgs.py
+++ stitchAugmentedImgs.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 import textwrap, os
 import os
@@ -15,57 +15,29 @@ def image_grid(imgs, rows, cols):
         grid.paste(img, box=(i%cols*w, i//cols*h))
     return grid
 
-
+def makeTextImage(text):
+    defaultImage = Image.new("RGB", (1920, 1080), (0, 0, 0))
+    draw = ImageDraw.Draw(defaultImage)
+    font = ImageFont.truetype("C:\\Windows\\Fonts\\arial.ttf", 200)
+    draw.text((1920/2, 1080/2 - (len(text) * 10)), text, (255, 255, 255), font=font)
+    return defaultImage
 choosenPhotos = ["001", "025", "082", "062", "017"]
 photosPaths = []
-#13
+#Add the 0 image
+
+photosPaths.append(makeTextImage("RANDOM IMAGES ON GOOGLE MEDIA PIPE"))
+for imgNumText in choosenPhotos:
+    photosPaths.append(makeTextImage("Image " + imgNumText))
 for imgNum in choosenPhotos:
     for augment in os.listdir("augmentedImgs"):
         im = Image.open("augmentedImgs/"+ augment + "/" + imgNum + "-" + augment + ".jpg", 'r')
         photosPaths.append(im)
-"""for imgNum in choosenPhotos:
-    imgStyles = []
-    for augment in os.listdir("augmentedImgs"):
-        imgStyles.append("augmentedImgs/" + imgNum + "-" + augment)
-    photosPaths.append(imgStyles)
-
-print(photosPaths[0])"""
-#image_grid(photosPaths, 5, 13)
-
-def display_images(
-    images, 
-    columns=5, width=20, height=8, max_images=15, 
-    label_wrap_length=50, label_font_size=8):
-
-    if not images:
-        print("No images to display.")
-        return 
-
-    if len(images) > max_images:
-        print(f"Showing {max_images} images of {len(images)}:")
-        images=images[0:max_images]
-
-    height = max(height, int(len(images)/columns) * height)
-    plt.figure(figsize=(width, height))
-    for i, image in enumerate(images):
-
-        plt.subplot(int(len(images) / columns + 1), columns, i + 1)
-        plt.imshow(image)
-
-        if hasattr(image, 'filename'):
-            title=image.filename
-            if title.endswith("/"): title = title[0:-1]
-            title=os.path.basename(title)
-            title=textwrap.wrap(title, label_wrap_length)
-            title="\n".join(title)
-            plt.title(title, fontsize=label_font_size); 
-#display_images(photosPaths)
 
 x,y = photosPaths[0].size
-ncol = 5
-nrow = 13
-cvs = Image.new('RGB',(x*ncol,y*nrow))
+ncol = 6
+nrow = 14
+gridImages = Image.new('RGB',(x * ncol, y * nrow))
 for i in range(len(photosPaths)):
-    px, py = x*(i%ncol), y*int(i/ncol)
-    cvs.paste(photosPaths[i],(px,py))
-cvs.save("tets.png")
+    px, py = x * (i % ncol), y * int(i/ncol)
+    gridImages.paste(photosPaths[i], (px, py))
+gridImages.save("allGridImages.png")
